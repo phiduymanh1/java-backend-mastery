b705bf1c673a720d6718be3e9b7ec9618a2138e3
PHI_MANH\manhp <phiduymanh.dev@gmail.com>
2026-07-17 17:01:14 +0700

    first commit

---
+++ microservice/Java-Microservice-Mastery/src/main/java/org/example/javamicroservicemastery/JavaMicroserviceMasteryApplication.java
@@ -0,0 +1,13 @@
+package org.example.javamicroservicemastery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMicroserviceMasteryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMicroserviceMasteryApplication.class, args);
+    }
+
+}
---
+++ microservice/Java-Microservice-Mastery/src/test/java/org/example/javamicroservicemastery/JavaMicroserviceMasteryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javamicroservicemastery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMicroserviceMasteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
